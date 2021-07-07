15a6b9431e160b083221ff58ec82991232da014c
rcroxyosox2 <rcroxyosox1@yahoo.com>
2021-07-07 00:39:57 -0700

    test

--- index.js
+++ index.js
@@ -198,12 +198,12 @@ app.get('/coin/:id', async(req, res) => {
 })
 
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client", "build")));
-
 app.get('/search', (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
